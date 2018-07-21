9a65fce859bf51852b75464ffc66d876043d0a6a
DESKTOP-87P3H06\Kaushik <kaushik@qseksolutions.com>
2018-07-21 10:31:02 +0530

    initial commit

---
+++ src/pages/list-master/list-master.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { IonicPage, ModalController, NavController } from 'ionic-angular';
+
+import { Item } from '../../models/item';
+import { Items } from '../../providers';
+
+@IonicPage()
+@Component({
+  selector: 'page-list-master',
+  templateUrl: 'list-master.html'
+})
+export class ListMasterPage {
+  cardItems: any[];
+  currentItems: Item[];
+
+  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
+    this.currentItems = this.items.query();
+    this.cardItems = [
+      {
+        user: {
+          avatar: 'assets/img/marty-avatar.png',
+          name: 'Marty McFly'
+        },
+        date: 'November 5, 1955',
+        image: 'assets/img/advance-card-bttf.png',
+        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+      },
+      {
+        user: {
+          avatar: 'assets/img/sarah-avatar.png.jpeg',
+          name: 'Sarah Connor'
+        },
+        date: 'May 12, 1984',
+        image: 'assets/img/advance-card-tmntr.jpg',
+        content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator, can learn the value of human life, maybe we can too.'
+      },
+      {
+        user: {
+          avatar: 'assets/img/ian-avatar.png',
+          name: 'Dr. Ian Malcolm'
+        },
+        date: 'June 28, 1990',
+        image: 'assets/img/advance-card-jp.jpg',
+        content: 'Your scientists were so preoccupied with whether or not they could, that they didn\'t stop to think if they should.'
+      }
+    ];
+  }
+
+  /**
+   * The view loaded, let's query our items for the list
+   */
+  ionViewDidLoad() {
+  }
+
+  /**
+   * Prompt the user to add a new item. This shows our ItemCreatePage in a
+   * modal and then adds the new item to our data source if the user created one.
+   */
+  addItem() {
+    let addModal = this.modalCtrl.create('ItemCreatePage');
+    addModal.onDidDismiss(item => {
+      if (item) {
+        this.items.add(item);
+      }
+    })
+    addModal.present();
+  }
+
+  /**
+   * Delete an item from the list of items.
+   */
+  deleteItem(item) {
+    this.items.delete(item);
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });
+  }
+}
