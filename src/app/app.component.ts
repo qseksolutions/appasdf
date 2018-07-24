84a3875ed85955987ffb24a2c1ffdcb783d15b27
praful <praful@qseksolutions.com>
2018-07-24 19:02:05 +0530

    add category list page and edit profile page update input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,8 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   pages: any[] = [
-    { title: 'Tutorial', component: 'TutorialPage' },
     { title: 'Welcome', component: 'WelcomePage' },
+    { title: 'Caregory', component: 'CaregoryPage' },
     { title: 'Tabs', component: 'TabsPage' },
     { title: 'Cards', component: 'CardsPage' },
     { title: 'Content', component: 'ContentPage' },
---
+++ src/pages/caregory/caregory.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CaregoryPage } from './caregory';
+
+@NgModule({
+  declarations: [
+    CaregoryPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CaregoryPage),
+  ],
+})
+export class CaregoryPageModule {}
