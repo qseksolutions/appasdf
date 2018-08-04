6dbad89044e114c914b898790ac5d6b70cf07b5c
DESKTOP-87P3H06\Kaushik <kaushik@qseksolutions.com>
2018-08-04 18:52:59 +0530

    last-design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,4 +70,10 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+  openHome() {
+      this.nav.setRoot('HomePage');
+  }
+  openProfile() {
+    this.nav.push('ProfilePage');
+  }
 }
